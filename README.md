1e6b5defa079daa1d29adc228432007851467404
Yuta Ide <sadness.ojisan@gmail.com>
2021-08-31 02:46:31 +0900

    md を html 表示する (#2)
    * copy text
    
    * show text area
    
    * build app

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # free-markdown
+
 free and secure mark down editor
+
+## development
+
+```
+trunk serve
+```
+
+## prd
+
+```
+trunk build
+```
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use yew::prelude::*;
+use yew::{html, Html, InputData,ShouldRender,ComponentLink,Component, web_sys};
+use yew::virtual_dom::{VNode};
+use yew::web_sys::{Node};
+use pulldown_cmark::{html as md_html, Options, Parser};
+
 
 enum Msg {
-    AddOne,
+    OnInput(String),
 }
 
 struct Model {
-    // `ComponentLink` is like a reference to a component.
-    // It can be used to send messages to the component
     link: ComponentLink<Self>,
-    value: i64,
+    input_value: String,
 }
 
 impl Component for Model {
@@ -17,19 +19,18 @@ impl Component for Model {
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
-            link,
-            value: 0,
+            input_value: "".into(),
+            link
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
+            Msg::OnInput(value) => {
+                let old_value = self.input_value.clone();
+                self.input_value = value;
                 true
-            }
+    }
         }
     }
 
@@ -41,10 +42,36 @@ impl Component for Model {
     }
 
     fn view(&self) -> Html {
+        let markdown_input_string = &self.input_value.clone();
+        let markdown_input = markdown_input_string.as_str();
+    println!("Parsing the following markdown string:\n{}", markdown_input);
+
+    // Set up options and parser. Strikethroughs are not part of the CommonMark standard
+    // and we therefore must enable it explicitly.
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(markdown_input, options);
+
+    // Write to String buffer.
+    let mut html_output: String = String::with_capacity(markdown_input.len() * 3 / 2);
+    md_html::push_html(&mut html_output, parser);
+
+
+    let div = web_sys::window()
+            .unwrap()
+            .document()
+            .unwrap()
+            .create_element("div")
+            .unwrap();
+    div.set_inner_html(&html_output);
+    let node = Node::from(div);
+        let vnode = VNode::VRef(node);
+
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+                <textarea value={self.input_value.clone()} 
+                oninput={self.link.callback(|e: InputData| Msg::OnInput(e.value))} />
+                {vnode}
             </div>
         }
     }
