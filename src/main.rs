096f1b4e2fe55c3ba4a1c8d596f5c07547318bcd
sadness_ojisan <sadness.ojisan@gmail.com>
2021-08-31 02:41:05 +0900

    show text area

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use yew::{html, Html, InputData,ShouldRender,ComponentLink,Component};
+use yew::{html, Html, InputData,ShouldRender,ComponentLink,Component, web_sys};
+use yew::virtual_dom::{VNode};
+use yew::web_sys::{Node};
+use pulldown_cmark::{html as md_html, Options, Parser};
+
 
 enum Msg {
     OnInput(String),
@@ -38,13 +42,36 @@ impl Component for Model {
     }
 
     fn view(&self) -> Html {
+        let markdown_input_string = &self.input_value.clone();
+        let markdown_input = markdown_input_string.as_str();
+    println!("Parsing the following markdown string:\n{}", markdown_input);
+
+    // Set up options and parser. Strikethroughs are not part of the CommonMark standard
+    // and we therefore must enable it explicitly.
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(markdown_input, options);
+
+    // Write to String buffer.
+    let mut html_output: String = String::with_capacity(markdown_input.len() * 3 / 2);
+    md_html::push_html(&mut html_output, parser);
+
+
+    let div = web_sys::window()
+            .unwrap()
+            .document()
+            .unwrap()
+            .create_element("div")
+            .unwrap();
+    div.set_inner_html(&html_output);
+    let node = Node::from(div);
+        let vnode = VNode::VRef(node);
+
         html! {
             <div>
-                <input value={self.input_value.clone()} 
+                <textarea value={self.input_value.clone()} 
                 oninput={self.link.callback(|e: InputData| Msg::OnInput(e.value))} />
-                <div>
-                {self.input_value.clone()}
-                </div>
+                {vnode}
             </div>
         }
     }
